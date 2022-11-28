eb8179caf9d22d45bd19aa8940dc9c5bc158ec38
sandeepk <karnatisandeep0@gmail.com>
2022-11-28 21:55:47 +0530

    intial commit for add DuplicateInArray

---
+++ src/practicejava/duplicateInArray.java
@@ -0,0 +1,33 @@
+package practicejava;
+
+import java.util.ArrayList;
+
+public class duplicateInArray {
+
+	public static void main(String[] args) {
+		String[] s= {"java","python","java"};
+		ArrayList<String> al=new ArrayList<String>();
+		for(int i=0;i<s.length; i++) {
+			int k=0;
+			if(!al.contains(s[i])) {
+				al.add(s[i]);
+				k++;
+				for(int j=i+1;j<s.length;j++) {
+					if(s[i]==s[j]) {
+						k++;
+					}
+					
+				}
+				if(k>1) 
+				System.out.println(s[i]+" "+k+"  "+"times");
+				else
+					System.out.println(s[i]+" "+k+"  "+"time");
+			}
+			
+			
+		}
+		
+		
+	}
+
+}
