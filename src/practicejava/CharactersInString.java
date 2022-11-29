7323aad7ac32841314e227d45e38635c036253c0
sandeepk <karnatisandeep0@gmail.com>
2022-11-29 18:12:56 +0530

    program to find no.of characters in array

---
+++ src/practicejava/CharactersInString.java
@@ -0,0 +1,26 @@
+package practicejava;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class CharactersInString {
+
+	public static void main(String[] args) {
+		String s="sandeep";
+		char[] chars=s.toCharArray();
+		HashMap<Character, Integer> hm=new HashMap<>();
+		for(Character ch:chars) {
+			if(hm.containsKey(ch)) {
+				hm.put(ch, hm.get(ch)+1);
+			}
+			else {
+				hm.put(ch, 1);
+			}
+		}
+		for(Character key:hm.keySet()) {
+			System.out.println(key+ "  "+hm.get(key));
+		}
+		
+	}
+
+}
